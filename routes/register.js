a2f903c2fdb362580455a0e137b5ce4faa0be219
unknown <paledirefilwe@gmail.com>
2023-10-11 11:23:44 +0200

    register

--- routes/register.js
+++ routes/register.js
@@ -3,13 +3,15 @@ const bodyparser=require('body-parser');
 const cors = require('cors');
 const router = express.Router();
 const app=express();
+const connection = require("../config/config");
+
 
 
 router.post('/register',(req, res)=>{
     const params = req.body;
     let sql ="'INSERT INTO user SET ?', params";
 
-    connection.query(sql, data,(err, results)=>{
+    connection.query('INSERT INTO user SET ?', params,(err, results)=>{
         if(err){
             return res.status(200).send("Failed to load data!"+err);
         }
