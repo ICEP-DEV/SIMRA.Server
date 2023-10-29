bc1adb4b90a5e320671c9087a66478b9c7d76426
unknown <joel.buti97@gmail.com>
2023-10-29 02:31:42 +0200

    update

--- routes/fib.js
+++ routes/fib.js
@@ -7,7 +7,8 @@ router.get('/indicators', (req, res)=>{
         {indicator:"Coliforms", ratio:1},
         {indicator:"E. coli", ratio:0.66},
         {indicator:"Enterococcus", ratio:0.01},
-        {indicator:"Clostridium", ratio:0.8}
+        {indicator:"Clostridium", ratio:0.8},
+        {indicator:"other"}
     ]
 
     res.send({message:"List of Indicators", success:true, indicators})
@@ -21,7 +22,7 @@ router.get('/reference_pathogens', (req, res)=>{
         {pathogen:"Salmonella typhi", best_fit_model:"beta-poisson", parameter:[{alpha:0.21, beta:49.78}]},
         {pathogen:"S.Flexneri", best_fit_model:"beta-poisson", parameter:[{alpha:0.265, beta:1480}]},
         {pathogen:"Vibrio Cholera", best_fit_model:"beta-poisson", parameter:[{alpha:0.169, beta:2305}]},
-        {pathogen:"Giardia lambia", best_fit_model:"exponential", parameter:[{constant:0.0199}]},
+        {pathogen:"Giardia lambia", best_fit_model:"exponential", parameter:[{constant:0.0199, n50:null}]},
         {pathogen:"Entamoeba coli", best_fit_model:"beta-poisson", parameter:[{alpha:0.101, n50:341}]}
     ]
 
--- routes/qmra.js
+++ routes/qmra.js
@@ -40,55 +40,84 @@ const calculateProbabiltyInfection = (probInfect, numofExposure) => {
     return calculatedResult;
 };
 
-router.post('/qmra', (req, res) => {
-
-    let organism = req.body.organism;
-    let fib = req.body.fib;
+router.post('/add_indicator_qmra', (req, res) => {
+    let is_customize_Pathogen = req.body.is_customize_Pathogen;
+    let pathogen = req.body.organism;
     let n50 = req.body.n50;
     let constant = req.body.constant;
     let alpha = req.body.alpha;
     let beta = req.body.beta;
-    let totalQmra = 0
-    var samplingId = req.body.samplingId
+    let totalQmra = 0;
+    var samplingId = req.body.samplingId;
+    let best_fit_model = req.body.best_fit_model
 
+    let indicator = req.body.indicator;
+    let ratio = req.body.ratio;
+    let count_indicator = req.body.count_indicator;
+    let estimated_count = req.body.estimated_count;
+    let is_customized = req.body.is_customizedIndicator;
 
-    switch (organism.toLocaleLowerCase()) {
+    switch (pathogen.toLocaleLowerCase()) {
         case 'Campylobacter jejuni'.toLocaleLowerCase():
-            totalQmra = calculateBetaPoisson(alpha, beta, fib)
+            totalQmra = calculateBetaPoisson(alpha, beta, count_indicator)
             break;
         case 'E.coli 0157:H7'.toLocaleLowerCase():
-            totalQmra = calculateBetaPoisson(alpha, beta, fib)
+            totalQmra = calculateBetaPoisson(alpha, beta, count_indicator)
             break;
         case 'Salmonella typhi'.toLocaleLowerCase():
-            totalQmra = calculateBetaPoisson(alpha, beta, fib)
+            totalQmra = calculateBetaPoisson(alpha, beta, count_indicator)
             break;
         case 'S.Flexneri'.toLocaleLowerCase():
-            totalQmra = calculateBetaPoisson(alpha, beta, fib)
+            totalQmra = calculateBetaPoisson(alpha, beta, count_indicator)
             break;
         case 'Vibrio Cholera'.toLocaleLowerCase():
-            totalQmra = calculateBetaPoisson(alpha, beta, fib)
+            totalQmra = calculateBetaPoisson(alpha, beta, count_indicator)
             break;
         case 'Entamoeba coli'.toLocaleLowerCase():
-            totalQmra = calculateEntamoebaColi(alpha, n50, fib)
+            totalQmra = calculateEntamoebaColi(alpha, n50, count_indicator)
             break;
         case 'Giardia lambia'.toLocaleLowerCase():
-            totalQmra = calculateExponentialForGiardia(constant, fib)
+            totalQmra = calculateExponentialForGiardia(constant, count_indicator)
             break;
     }
-    var date = new Date()
 
-    var durationType = req.body.durationType;
-    var probability = (1 - (1 - totalQmra)) ** (-durationType)
-    var qmraBody = [totalQmra, date, samplingId]
-    var sql = `INSERT INTO qmra(pi,dateCreated,samplingId)
-            VALUES(?,?,?)`;
-    connection.query(sql, qmraBody, (err, results) => {
+    var duration_type = req.body.duration_type;
+    //var probability = (1 - (1 - totalQmra)) ** (-durationType)
+    var likeliOfInfection = null
+    // prepare insetion of FIB indicatore
+    var fibIndicatorBody = [indicator, ratio, count_indicator, estimated_count, is_customized, samplingId]
+    var fib_sql = `INSERT INTO fib_indicator(indicator,ratio,count_indicator,estimated_count,is_customized,samplingId)
+                VALUES(?,?,?,?,?,?)`;
+
+    connection.query(fib_sql, fibIndicatorBody, (err, results) => {
         if (err) {
             return res.status(200).send("Failed to load data!" + err);
         }
         else {
+            console.log(results)
             if (results.affectedRows > 0) {
-                res.status(200).json({ totalQmra, probability, organism, success: true });
+                var indicator_id = results.insertId
+                // Prepare insertion of QMRA
+                var qmra_body = [pathogen, best_fit_model, alpha, beta, constant, n50, totalQmra, likeliOfInfection, duration_type, is_customize_Pathogen, indicator_id]
+                var qmra_sql = `INSERT INTO qmra(pathogen,best_fit_model,alpha,beta,constant,n50,probability_of_infection,likelihood_of_infection,duration_type,is_customized,indicator_id)
+                                VALUES(?,?,?,?,?,?,?,?,?,?,?)`
+                connection.query(qmra_sql, qmra_body, (error, row) => {
+                    console.log('third test')
+
+                    if (error) {
+                        console.log(error)
+                        throw err
+                    };
+                    console.log('fifth test')
+
+                    if (row.affectedRows > 0) {
+                        console.log('fourth test')
+
+                        var qmra_id = row.insertId
+                        res.send({ success: true, totalQmra, qmra_id })
+                    }
+                })
+
             }
             else {
                 res.status(200).json({ success: false, message: "Something went wrong try again later" });
@@ -97,7 +126,45 @@ router.post('/qmra', (req, res) => {
         }
     });
 
+})
 
+router.put('/likelihood_test/:qmra_id', (req, res) => {
+    var duration_type = req.body.duration_type;
+    var probability_of_infection = req.body.probability_of_infection
+    var duration_number = 0;
+    if(duration_type.toLocaleLowerCase() === 'daily'){
+        duration_number = 31
+    }
+    else if(duration_type.toLocaleLowerCase() === 'wekkly'){
+        duration_number = 54
+    }
+    else if(duration_type.toLocaleLowerCase() === 'monthly'){
+        duration_number = 12
+    }
+    else if(duration_type.toLocaleLowerCase() === 'quartely'){
+        duration_number = 4
+    }
+    else {
+        duration_number = 1
+    }
+    var likelihood_of_infection = (1 - (1 - probability_of_infection)) ** (-duration_number)
+    var likelihood_body = [likelihood_of_infection, req.params.qmra_id]
+    var sql = `UPDATE qmra
+                SET likelihood_of_infection = ?
+                WHERE qmra_id = ?;`
+    connection.query(sql, likelihood_body, (err, results)=>{
+        if(err) throw err;
+        if(results.affectedRows > 0){
+            res.send({success:true, likelihood_of_infection})
+        }
+        else{
+            res.send({success:false, message:"could not perform likelihood of infection"})
+        }
+    })
+})
+
+router.post('/mst', (req, res)=>{
+    
 })
 
 router.get('/qmra_group_results', (req, res) => {
