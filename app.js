069e141a5bae8e1311781eeb1f3eac08e72950c9
unknown <joel.buti97@gmail.com>
2023-11-13 06:24:00 +0200

    update

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(bodyParser.json()); 
 
 app.use(bodyParser.urlencoded({ extended: false }));
+/*
 app.use(cookieParser());
 
 
@@ -29,7 +30,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: { maxAge: oneDay }
 }));
-
+*/
 app.use('/api', require('./routes/user'));
 app.use('/api', require('./routes/level1'));
 app.use('/api', require('./routes/fib'));
