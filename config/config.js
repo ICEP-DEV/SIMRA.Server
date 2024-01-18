df20730f4d0575782e5fb551431bdf6de1d92a04
unknown <joel.buti97@gmail.com>
2024-01-18 21:38:13 +0200

    update

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
 });
 
 */
-/*
+
 var connection = mysql.createConnection({
     host: 'simra-db.cwtgw4wh8ldi.eu-west-1.rds.amazonaws.com',
     user: 'admin',
@@ -16,32 +16,15 @@ var connection = mysql.createConnection({
     database: 'simra_db',
     port: 3306
 });
-*/
 
 
-/*
+
+
 connection.connect((err) => {
     if (!err)
         console.log('Connection Established Successfully');
     else
         console.log('Connection Failed!' + JSON.stringify(err, undefined, 2));
 });
-*/
 
-const connection = mysql.createPool({
-    host: 'simra-db.cwtgw4wh8ldi.eu-west-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'simra.tut.ac.za',
-    database: 'simra_db',
-    port: 3306,
-    connectionLimit:2000
-})
-connection.getConnection((err, con) => {
-    if (err) 
-    console.log('Connection Failed!' + JSON.stringify(err, undefined, 2));
-    if(con.state === 'disconnected')
-        console.log(`Database connection failed. Check that its running and that your configuration is correct`);
-    else 
-    console.log('Connection Established Successfully');
-});
 module.exports = connection;
